81ecc0c93e74591dcbe70ac38d066c8901090eba
Queenzy-Ongeye <queenzyblack176@gmail.com>
2024-07-24 08:28:26 +0300

    feat: Initialize the useNavigate hook from react-router-dom to programmatically navigate between routes.

--- src/components/BleButtons/BleButtons.jsx
+++ src/components/BleButtons/BleButtons.jsx
@@ -1,16 +1,21 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const BleButtons = ({ startBleScan, stopBleScan, toastMsg }) => {
+  const navigate = useNavigate();
+
+
+
   return (
     <div className="absolute inset-0 flex flex-col justify-around items-center bg-black">
       <div id="app" className="flex-1 h-4/5 flex flex-col flex-wrap mt-2">
-        <button className="w-60 h-14 rounded-md bg-slate-50" onClick={startBleScan}>
+        <button className="w-64 h-16 rounded-md bg-slate-50" onClick={handleStartBleScan}>
           startBleScan
         </button>
-        <button className="w-60 h-14 rounded-md mt-2 bg-slate-50" onClick={stopBleScan}>
+        <button className="w-64 h-16 rounded-md mt-2 bg-slate-50" onClick={stopBleScan}>
           stopBleScan
         </button>
-        <button className="w-60 h-14 rounded-md mt-2 bg-slate-50" onClick={toastMsg}>
+        <button className="w-64 h-16 rounded-md mt-2 bg-slate-50" onClick={toastMsg}>
           toastMsg
         </button>
       </div>
