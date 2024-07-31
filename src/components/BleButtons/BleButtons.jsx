8cb00ca018be2e5d96a6b2238f7ea9422b31ab8f
Queenzy-Ongeye <queenzyblack176@gmail.com>
2024-07-31 15:15:30 +0300

    solved merge conflicts

--- src/components/BleButtons/BleButtons.jsx
+++ src/components/BleButtons/BleButtons.jsx
@@ -15,83 +15,12 @@ const BleButtons = ({
   const [macAddress, setMacAddress] = useState("");
   const navigate = useNavigate();
 
-  const columnsData = [
-    {
-      Header: "No",
-      accessor: "",
-      Cell: ({ row }) => (
-        <span className="py-3.5 px-4 text-sm font-medium text-gray-700">
-          {row.index + 1}
-        </span>
-      ),
-      sortType: "basic",
-    },
-    {
-      Header: "Address",
-      accessor: "macAddress",
-      sortType: "basic",
-      Cell: ({ value }) => (
-        <span className="py-3.5 px-4 text-sm font-medium text-gray-800">
-          {value}
-        </span>
-      ),
-    },
-    {
-      Header: "Full Name",
-      accessor: "fullName",
-      sortType: "basic",
-      Cell: ({ value }) => (
-        <span className="py-3.5 px-4 text-sm font-medium text-gray-800">
-          {value}
-        </span>
-      ),
-    },
-    {
-      Header: "Product Name",
-      accessor: "productName",
-      sortType: "basic",
-      Cell: ({ value }) => (
-        <span className="py-3.5 px-4 text-sm font-medium text-gray-800">
-          {value}
-        </span>
-      ),
-    },
-    {
-      Header: "Product ID",
-      accessor: "productId",
-      sortType: "basic",
-      Cell: ({ value }) => (
-        <span className="py-3.5 px-4 text-sm font-medium text-gray-800">
-          {value}
-        </span>
-      ),
-    },
-    {
-      Header: "TimeStamp",
-      accessor: "timestampNanos",
-      sortType: "basic",
-      Cell: ({ value }) => (
-        <span className="py-3.5 px-4 text-sm font-medium text-gray-800">
-          {value}
-        </span>
-      ),
-    },
-    {
-      Header: "Connection",
-      accessor: "isConnected",
-      Cell: ({ value }) => (
-        <span
-          className={`${
-            value
-              ? "bg-green-500 text-white p-2 rounded-md"
-              : "bg-red-500 text-white p-2 rounded-md"
-          }`}
-        >
-          {value ? "Connected" : "Not Connected"}
-        </span>
-      ),
-      sortType: "basic",
-    },
+  const handleViewClick = (deviceData) => {
+    navigate(`/device-details/${deviceData.macAddress}`, { state: deviceData });
+  };
+
+  const columnsWithViewButton = [
+    ...columnsData,
     {
       Header: "Actions",
       accessor: "actions",
