ae621e83445a4edb2451f300eae2ad4b2398ae1d
Queenzy-Ongeye <queenzyblack176@gmail.com>
2024-08-05 17:44:26 +0300

    Checked that the reducer correctly handles the `SET_DETECTED_DEVICES` action.

--- src/service/store.js
+++ src/service/store.js
@@ -1,35 +1,27 @@
 import React, { createContext, useReducer, useContext } from "react";
 
 const initialState = {
-  bridgeInitialized: false,
-  bleData: [],
-  isScanning: false,
-  detectedDevices: [],
-  keyword: "OVES",
-  selectedMacAddress: "",
-};
-
-const reducer = (state, action) => {
-  switch (action.type) {
-    case "SET_BRIDGE_INITIALIZED":
-      return { ...state, bridgeInitialized: action.payload };
-    case "SET_BLE_DATA":
-      return { ...state, bleData: [...state.bleData, action.payload] };
-    case "SET_IS_SCANNING":
-      return { ...state, isScanning: action.payload };
-    case "SET_DETECTED_DEVICES":
-      return {
-        ...state,
-        detectedDevices: [...state.detectedDevices, action.payload],
-      };
-    case "SET_KEYWORD":
-      return { ...state, keyword: action.payload };
-    case "SET_SELECTED_MAC_ADDRESS":
-      return { ...state, selectedMacAddress: action.payload };
-    default:
-      return state;
-  }
-};
+    bridgeInitialized: false,
+    isScanning: false,
+    bleData: [],
+    detectedDevices: [],
+  };
+  
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case "SET_BRIDGE_INITIALIZED":
+        return { ...state, bridgeInitialized: action.payload };
+      case "SET_IS_SCANNING":
+        return { ...state, isScanning: action.payload };
+      case "SET_BLE_DATA":
+        return { ...state, bleData: action.payload };
+      case "SET_DETECTED_DEVICES":
+        return { ...state, detectedDevices: [...state.detectedDevices, action.payload] };
+      default:
+        return state;
+    }
+  };
+  
 
 const StoreContext = createContext();
 
