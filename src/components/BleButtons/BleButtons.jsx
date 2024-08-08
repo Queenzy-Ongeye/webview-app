7677a8b73f3a16c96d2a794dd71e650898d3363c
Queenzy-Ongeye <queenzyblack176@gmail.com>
2024-08-08 13:25:16 +0300

    added header

--- src/components/BleButtons/BleButtons.jsx
+++ src/components/BleButtons/BleButtons.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useStore } from "../../service/store";
 import { useNavigate } from "react-router-dom";
-import { AiOutlineLoading3Quarters } from 'react-icons/ai';
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
 
 const BleButtons = ({
   startBleScan,
@@ -15,7 +15,6 @@ const BleButtons = ({
 }) => {
   const { state, dispatch } = useStore();
   const navigate = useNavigate();
-  const [loading, setLoading] = useState(false); // State for loading spinner
   const [connectingMacAddress, setConnectingMacAddress] = useState(null); // State for the device being connected
   const [initializingMacAddress, setInitializingMacAddress] = useState(null); // State for the device being initialized
 
