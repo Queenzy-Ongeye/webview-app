7a1cec2397b14ff7540725085cecf6153b7ead18
Queenzy-Ongeye <queenzyblack176@gmail.com>
2024-08-08 11:09:22 +0300

    made changes

--- src/components/DeviceDetails/STSPage.jsx
+++ src/components/DeviceDetails/STSPage.jsx
@@ -1,18 +1,40 @@
-import React from 'react';
-import DataDisplay from './DataDisplay';
+// src/AttPage.js
+import React from "react";
+import { useLocation } from "react-router-dom";
 
-const STSPage = ({ data }) => {
+const STSPage = () => {
+  const location = useLocation();
+  const { data } = location.state || {};
+
+  const renderCharacterMap = (characterMap) => {
+    return Object.entries(characterMap).map(([uuid, char]) => (
+      <div key={uuid} className="characteristic">
+        <h4>UUID: {uuid}</h4>
+        <p>Description: {char.desc}</p>
+        <p>Real Value: {char.realVal}</p>
+        <p>Service UUID: {char.serviceUuid}</p>
+        <p>Value Type: {char.valType}</p>
+        <p>Values: {char.values.join(", ")}</p>
+        <p>Properties: {char.properties}</p>
+        <p>Enable Notify: {char.enableNotify.toString()}</p>
+        <p>Enable Read: {char.enableRead.toString()}</p>
+      </div>
+    ));
+  };
+
   return (
-    <div className="data-tab">
-      <h2>STS Data</h2>
-      {data.map((item, index) => (
-        <DataDisplay
-          key={index}
-          title={item.name}
-          value={item.realVal}
-          description={item.desc}
-        />
-      ))}
+    <div className="sts-page">
+      {data &&
+        data.map((service, index) => (
+          <div key={index} className="service">
+            <h3>Service UUID: {service.uuid}</h3>
+            <h3>Service Name Enum: {service.serviceNameEnum}</h3>
+            <h3>Service Property: {service.serviceProperty}</h3>
+            <div className="characteristics">
+              {renderCharacterMap(service.characterMap)}
+            </div>
+          </div>
+        ))}
     </div>
   );
 };
