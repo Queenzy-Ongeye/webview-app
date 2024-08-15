715873e8071efcbe545958d70d9c381a41ddd7ef
Queenzy-Ongeye <queenzyblack176@gmail.com>
2024-08-15 22:37:06 +0300

    Add service worker for offline support

---
+++ src/service/service-worker.js
@@ -0,0 +1,60 @@
+const CACHE_NAME = "ble-app-cache-v1";
+const urlsToCache = [
+    "/",
+    "/public/index.html",
+    "/src/App.css",
+    "/src/App.js",
+    "/src/Home.js",
+    "/src/store.js",
+    "/src/components/BleButtons/BleButtons.jsx",
+    "/src/components/DeviceDetails/ATTPage.jsx",
+    "/src/components/DeviceDetails/STSPage.jsx",
+    "/src/components/DeviceDetails/CMDPage.jsx",
+    "/src/components/DeviceDetails/DTAPage.jsx",
+    "/src/components/DeviceDetails/DIAPage.jsx"
+];
+
+self.addEventListener("install", (event) =>{
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) =>{
+            console.log("Opened cache");
+            return cache.addAll(urlsToCache)
+        })
+    );
+});
+
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) =>{
+            if (response) {
+                return response;
+            }
+            return fetch(event.request).then((response) =>{
+                // Checking if response received is valid
+                if(!response || response.status !== 200 || response.type !== "basic"){
+                    return response;
+                }
+                const responseToCache = response.clone();
+                caches.open(CACHE_NAME).then((cache) => {
+                    cache.put(event.request, responseToCache);
+                });
+                return response;
+            });
+        })
+    );
+});
+
+self.addEventListener("activate", (event) => {
+    const cacheWhiteList = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then((cacheNames) =>{
+            return Promise.all(
+                cacheNames.map((cacheName) =>{
+                    if(cacheWhiteList.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            )
+        })
+    )
+})
