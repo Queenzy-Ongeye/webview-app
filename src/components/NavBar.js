e6e03477608f781e6110d9d51e45164bcd12b2e9
Queenzy-Ongeye <queenzyblack176@gmail.com>
2024-08-19 15:20:57 +0300

    added navbar and bottom action bar

---
+++ src/components/NavBar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const NavigationBar = () => {
+  return (
+    <nav className="bg-cyan-500 text-white py-4">
+      <div className="container mx-auto flex justify-around">
+        <Link to="/" className="px-4 py-2 hover:bg-cyan-600 rounded">
+          Home
+        </Link>
+        <Link to="/ble-scan" className="px-4 py-2 hover:bg-cyan-600 rounded">
+          BLE Scan
+        </Link>
+        <Link to="/qr-code-scan" className="px-4 py-2 hover:bg-cyan-600 rounded">
+          QR Code Scan
+        </Link>
+      </div>
+    </nav>
+  );
+};
+
+export default NavigationBar;
