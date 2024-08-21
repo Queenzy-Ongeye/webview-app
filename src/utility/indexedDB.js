b5f392296d6ce4c3d9c08c3d779205ae93ebcfd9
Queenzy-Ongeye <queenzyblack176@gmail.com>
2024-08-21 10:06:04 +0300

    fix: fixed store data object name

--- src/utility/indexedDB.js
+++ src/utility/indexedDB.js
@@ -3,99 +3,99 @@ const DB_VERSION = 1;
 const STORE_NAME = "bleDataStore";
 
 function openDB() {
-    return new Promise((resolve, reject) => {
-        const request = indexedDB.open(DB_NAME, DB_VERSION);
+  return new Promise((resolve, reject) => {
+    const request = indexedDB.open(DB_NAME, DB_VERSION);
 
-        request.onupgradeneeded = (event) => {
-            const db = event.target.result;
-            if(!db.objectStoreNames.contains(STORE_NAME)) {
-                db.createObjectStoreName(STORE_NAME, { keyPath: "id", autoIncrement: true});
+    request.onupgradeneeded = (event) => {
+      const db = event.target.result;
+      if (!db.objectStoreNames.contains(STORE_NAME)) {
+        db.createObjectStore(STORE_NAME, {
+          keyPath: "id",
+          autoIncrement: true,
+        });
+      }
+    };
 
-            }
-        };
+    request.onsuccess = (event) => {
+      resolve(event.target.result);
+    };
 
-        request.onsuccess = (event) => {
-            resolve(event.target.result)
-        };
-
-        request.onerror =(event) => {
-            reject(event.target.error)
-        };
-    });
-};
+    request.onerror = (event) => {
+      reject(event.target.error);
+    };
+  });
+}
 
 function addData(data) {
-    return openDB().then((db) => {
-        return new Promise((resolve, reject) => {
-            const transaction = db.transaction(STORE_NAME, "readwrite");
-            const store = transaction.objectStore(STORE_NAME);
-            const request = store.add(data);
+  return openDB().then((db) => {
+    return new Promise((resolve, reject) => {
+      const transaction = db.transaction(STORE_NAME, "readwrite");
+      const store = transaction.objectStore(STORE_NAME);
+      const request = store.add(data);
 
-            request.onsuccess = () => {
-                resolve(request.result);
-            };
+      request.onsuccess = () => {
+        resolve(request.result);
+      };
 
-            request.onerror = (event) => {
-                reject(event.target.erro)
-            };
-
-        });
+      request.onerror = (event) => {
+        reject(event.target.error);
+      };
     });
-};
+  });
+}
 
 function getData(id) {
-    return openDB().then((db) => {
-        return new Promise((resolve, reject) => {
-            const transaction = db.transaction(STORE_NAME, "readonly");
-            const store = transaction.objectStore(STORE_NAME);
-            const request = store.objectStore(STORE_NAME);
+  return openDB().then((db) => {
+    return new Promise((resolve, reject) => {
+      const transaction = db.transaction(STORE_NAME, "readonly");
+      const store = transaction.objectStore(STORE_NAME);
+      const request = store.get(id);
 
-            request.onsuccess = () =>{
-                resolve(request.result);
-            };
+      request.onsuccess = () => {
+        resolve(request.result);
+      };
 
-            request.onerror = (event) => {
-                reject(event.target.error)
-            };
-        });
+      request.onerror = (event) => {
+        reject(event.target.error);
+      };
     });
-};
+  });
+}
 
-function getAllData(){
-    return openDB().then((db) =>{
-        return new Promise((resolve, reject) => {
-            const transaction = db.transaction(STORE_NAME, "readonly");
-            const store = transaction.objectStore(STORE_NAME);
-            const request = store.objectStore(STORE_NAME);
-            
-            request.onsuccess = () => {
-                resolve(request.result);
-            };
+function getAllData() {
+  return openDB().then((db) => {
+    return new Promise((resolve, reject) => {
+      const transaction = db.transaction(STORE_NAME, "readonly");
+      const store = transaction.objectStore(STORE_NAME);
+      const request = store.getAll();
 
-            request.onerror = (event) => {
-                reject(event.target.error);
-            };
-        });
+      request.onsuccess = () => {
+        resolve(request.result);
+      };
+
+      request.onerror = (event) => {
+        reject(event.target.error);
+      };
     });
-};
+  });
+}
 
 function deleteData(id) {
-    return openDB().then((db) => {
-        return new Promise((resolve, reject) => {
-            const transaction= db.transaction(STORE_NAME, "readwrite");
-            const store = transaction.objectStore(STORE_NAME);
-            const request = store.delete(id)
+  return openDB().then((db) => {
+    return new Promise((resolve, reject) => {
+      const transaction = db.transaction(STORE_NAME, "readwrite");
+      const store = transaction.objectStore(STORE_NAME);
+      const request = store.delete(id);
 
-            request.onsuccess = () => {
-                resolve();
-            };
+      request.onsuccess = () => {
+        resolve();
+      };
 
-            request.onerror = (event) => {
-                reject(event.target.error);
-            };
-        });
+      request.onerror = (event) => {
+        reject(event.target.error);
+      };
     });
+  });
 };
 
-export {addData, getData, getAllData, deleteData}
-
+export { addData, getData, getAllData, deleteData };
