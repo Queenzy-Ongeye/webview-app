d19a8bfb9877d48d0a6073d9ad3a719c90a3fc20
Queenzy-Ongeye <queenzyblack176@gmail.com>
2024-08-25 18:12:47 +0300

    changed stylying

--- src/components/BleButtons/BottomActionBar.js
+++ src/components/BleButtons/BottomActionBar.js
@@ -7,7 +7,7 @@ const BottomActionBar = ({ onStartScan, onStopScan, onScanData, isScanning }) =>
       <Button
         onClick={onStartScan}
         disabled={isScanning}
-        className={isScanning ? 'bg-gray-500 text-white' : 'bg-white text-cyan-700'}
+        className={isScanning ? 'bg-gray-700 text-white' : 'bg-white text-cyan-700'}
       >
         {isScanning ? 'Scanning...' : 'Start BLE Scan'}
       </Button>
