7f40eba4751b158806abec4767c8f4c68756cd9c
Queenzy-Ongeye <queenzyblack176@gmail.com>
2024-09-04 11:51:43 +0300

    removed MQTT intergration function

--- src/Home.js
+++ src/Home.js
@@ -4,7 +4,6 @@ import { useStore } from "./service/store";
 import BottomActionBar from "./components/BleButtons/BottomActionBar";
 import { getAllData, getDataByBarcode } from "./utility/indexedDB";
 import { useNavigate } from "react-router-dom";
-import mqtt from "mqtt";
 
 const Home = () => {
   const { state, dispatch } = useStore();
@@ -157,34 +156,6 @@ const Home = () => {
     connectWebViewJavascriptBridge(setupBridge);
   }, [state.bridgeInitialized, dispatch]);
 
-  // MQTT Data intergration
-  useEffect(() => {
-    const options = {
-      username: "Scanner2",
-      password: "!mqttsc.2024#",
-      clientId: 'emqx_MDY1Mz'
-    };
-    const client = mqtt.connect("wss://emqx.omnivoltaic.com:8084/mqtt", options);
-
-    client.on("connect", () => {
-      console.log("Connected to MQTT broker");
-    });
-
-    client.on("error", (err) => {
-      console.error("MQTT connection error:", err.message || err);
-    });
-
-    client.on("disconnect", () => {
-      console.log("Disconneted to mqtt");
-    });
-
-    dispatch({ type: "SET_MQTT_CLIENT", payload: client });
-
-    return () => {
-      if (client) client.end();
-    };
-  }, [dispatch]);
-
   const startBleScan = () => {
     if (window.WebViewJavascriptBridge) {
       window.WebViewJavascriptBridge.callHandler(
@@ -311,7 +282,7 @@ const Home = () => {
         (responseData) => {
           console.log("Response from startQrCodeScan", responseData);
           dispatch({ type: "SET_QR_DATA", payload: responseData });
-          navigate("/scan-data", { state: { scannedData: responseData } });
+          navigate("/scan-data", {state: {scannedData: respo}})
         }
       );
       dispatch({ type: "SET_QR_SCANNING", payload: true });
@@ -374,45 +345,9 @@ const Home = () => {
       });
   };
 
-  // Publishing mqtt data
-  const publishMqttData = (topic, message) => {
-    const client = state.mqttClient;
-    if (client) {
-      client.publish(topic, message, { qos: 1 }, (err) => {
-        if (err) {
-          console.error("Failed to publish message:", err);
-        } else {
-          console.log(
-            `Message "${message}" successfully published to topic "${topic}"`
-          );
-        }
-      });
-    }
+  const handleSettings = () => {
+    alert("Settings selected");
   };
-
-  useEffect(() => {
-    if (state.initBleData) {
-      console.log("Publishing MQTT data:", state.initBleData); // Add this line to log the data
-      if (state.initBleData.ATT) {
-        publishMqttData("emit/bleData/att", JSON.stringify(state.initBleData.ATT));
-      }
-      if (state.initBleData.STS) {
-        publishMqttData("emit/bleData/sts", JSON.stringify(state.initBleData.STS));
-      }
-      if (state.initBleData.CMD) {
-        publishMqttData("emit/bleData/cmd", JSON.stringify(state.initBleData.CMD));
-      }
-      if (state.initBleData.DIA) {
-        publishMqttData("emit/bleData/dia", JSON.stringify(state.initBleData.DIA));
-      }
-      if (state.initBleData.DTA) {
-        publishMqttData("emit/bleData/dta", JSON.stringify(state.initBleData.DTA));
-      }
-    } else {
-      console.error("No BLE data available to publish to MQTT."); // Log this if there's no data
-    }
-  }, [state.initBleData]);
-  
 
   console.log("State in Home component:", state);
 
