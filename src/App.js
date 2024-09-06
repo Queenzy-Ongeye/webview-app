e7d56d28ec0ecff3e1ea8f561023411331eadf9d
Queenzy-Ongeye <queenzyblack176@gmail.com>
2024-09-06 09:49:58 +0300

    removed theme provider

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Suspense, lazy } from "react";
-import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import { StoreProvider } from "./service/store";
 import NavigationBar from "./components/NavBar";
 import { AiOutlineLoading3Quarters } from "react-icons/ai";
@@ -16,32 +16,30 @@ const ScanData = lazy(() => import("./components/scanQr-Barcode/ScanData"));
 const App = () => {
   return (
     <StoreProvider>
-      <ThemeProvider>
-        <Router>
-          <div className="min-h-screen flex">
-            <NavigationBar />
-            <div className="flex-grow">
-              <Suspense
-                fallback={
-                  <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
-                    <AiOutlineLoading3Quarters className="animate-spin h-10 w-10 text-white" />
-                  </div>
-                }
-              >
-                <Routes>
-                  <Route path="/" element={<Home />} />
-                  <Route path="/att" element={<AttPage />} />
-                  <Route path="/cmd" element={<CMDPage />} />
-                  <Route path="/sts" element={<StsPage />} />
-                  <Route path="/dta" element={<DTAPage />} />
-                  <Route path="/dia" element={<DIAPage />} />
-                  <Route path="/scan-data" element={<ScanData />} />
-                </Routes>
-              </Suspense>
-            </div>
+      <Router>
+        <div className="min-h-screen flex">
+          <NavigationBar />
+          <div className="flex-grow">
+            <Suspense
+              fallback={
+                <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
+                  <AiOutlineLoading3Quarters className="animate-spin h-10 w-10 text-white" />
+                </div>
+              }
+            >
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/att" element={<AttPage />} />
+                <Route path="/cmd" element={<CMDPage />} />
+                <Route path="/sts" element={<StsPage />} />
+                <Route path="/dta" element={<DTAPage />} />
+                <Route path="/dia" element={<DIAPage />} />
+                <Route path="/scan-data" element={<ScanData />} />
+              </Routes>
+            </Suspense>
           </div>
-        </Router>
-      </ThemeProvider>
+        </div>
+      </Router>
     </StoreProvider>
   );
 };
