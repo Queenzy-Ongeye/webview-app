2063464702fa752d8f7453efebc4542f03edb932
Queenzy-Ongeye <queenzyblack176@gmail.com>
2024-09-13 12:20:59 +0300

    added topic

--- src/Home.js
+++ src/Home.js
@@ -124,6 +124,180 @@ const Home = () => {
     }
   };
 
+  const startBleScan = () => {
+    if (window.WebViewJavascriptBridge) {
+      window.WebViewJavascriptBridge.callHandler(
+        "startBleScan",
+        "",
+        (responseData) => {
+          try {
+            const jsonData = JSON.parse(responseData);
+            dispatch({ type: "SET_BLE_DATA", payload: jsonData });
+          } catch (error) {
+            console.error(
+              "Error parsing JSON data from 'startBleScan' response:",
+              error
+            );
+          }
+        }
+      );
+      dispatch({ type: "SET_IS_SCANNING", payload: true });
+    } else {
+      console.error("WebViewJavascriptBridge is not initialized.");
+    }
+  };
+
+  const stopBleScan = () => {
+    if (window.WebViewJavascriptBridge && state.isScanning) {
+      window.WebViewJavascriptBridge.callHandler("stopBleScan", "", () => {
+        console.log("Scanning stopped");
+      });
+      dispatch({ type: "SET_IS_SCANNING", payload: false });
+    } else {
+      console.error(
+        "WebViewJavascriptBridge is not initialized or scanning is not active."
+      );
+    }
+  };
+
+  const connectToBluetoothDevice = (macAddress) => {
+    if (window.WebViewJavascriptBridge) {
+      window.WebViewJavascriptBridge.callHandler(
+        "connBleByMacAddress",
+        macAddress,
+        (responseData) => {
+          try {
+            const parsedData = JSON.parse(responseData);
+            if (parsedData.respCode === "200") {
+              initBleData(macAddress);
+            }
+            dispatch({ type: "SET_BLE_DATA", payload: parsedData });
+          } catch (error) {
+            console.error(
+              "Error parsing JSON data from 'connBleByMacAddress' response:",
+              error
+            );
+          }
+        }
+      );
+    } else {
+      console.error("WebViewJavascriptBridge is not initialized.");
+    }
+  };
+
+  const initBleData = (macAddress) => {
+    if (window.WebViewJavascriptBridge) {
+      window.WebViewJavascriptBridge.callHandler(
+        "initBleData",
+        macAddress,
+        (responseData) => {
+          try {
+            const parsedData = JSON.parse(responseData);
+            dispatch({ type: "SET_INIT_BLE_DATA", payload: parsedData });
+          } catch (error) {
+            console.error(
+              "Error parsing JSON data from 'initBleData' response:",
+              error
+            );
+          }
+        }
+      );
+    } else {
+      console.error("WebViewJavascriptBridge is not initialized.");
+    }
+  };
+
+  const startQrCode = () => {
+    if (window.WebViewJavascriptBridge) {
+      // Trigger native QR/Barcode scan via the bridge
+      window.WebViewJavascriptBridge.callHandler(
+        "startQrCodeScan",
+        999, // Arbitrary request ID
+        (responseData) => {
+          try {
+            console.log("Response from startQrCodeScan", responseData);
+
+            // Parse the nested response data
+            const parsedData = JSON.parse(responseData.data);
+
+            if (
+              !parsedData ||
+              !parsedData.respData ||
+              !parsedData.respData.value
+            ) {
+              throw new Error("No valid QR or barcode scan data received");
+            }
+
+            const scannedValue = parsedData.respData.value; // Extract the scanned value (barcode/QR code)
+            console.log("Scanned Value:", scannedValue);
+
+            // Process the scanned data to check whether it's a QR code or barcode
+            handleScanData(scannedValue);
+          } catch (error) {
+            console.error("Error during QR/Barcode scan:", error.message);
+          }
+        }
+      );
+      // Update the state to show scanning is active
+      dispatch({ type: "SET_QR_SCANNING", payload: true });
+    } else {
+      console.error("WebViewJavascriptBridge is not initialized.");
+    }
+  };
+
+  const handleScanData = (data) => {
+    console.log("Scanned data received: ", data);
+
+    // Check if it's a barcode
+    if (isBarcode(data)) {
+      fetchProductDetails(data); // Process barcode to fetch product details
+    }
+    // Otherwise, it should be a QR code
+    else if (isQrCode(data)) {
+      dispatch({ type: "SET_QR_DATA", payload: data }); // Save QR code data in state
+    } else {
+      console.error("Invalid scan data. Neither a barcode nor a QR code.");
+    }
+  };
+
+  // Function to check if data is a barcode (numeric string with specific lengths)
+  const isBarcode = (data) => {
+    const numericPattern = /^[0-9]+$/; // Only numeric values
+    const barcodeLengths = [8, 12, 13]; // Common barcode lengths (UPC, EAN)
+    return numericPattern.test(data) && barcodeLengths.includes(data.length);
+  };
+
+  // Function to check if data is a QR code (URLs or structured text)
+  const isQrCode = (data) => {
+    const urlPattern = /^(http|https):\/\/[^ "]+$/; // Checks if it's a URL
+    const structuredDataPattern =
+      /^[a-zA-Z0-9]+=[a-zA-Z0-9]+(&[a-zA-Z0-9]+=[a-zA-Z0-9]+)*$/; // Checks if it's structured text
+    const nonNumericPattern = /[^0-9]/; // Ensures it's not purely numeric
+
+    // Check common QR code patterns
+    return (
+      urlPattern.test(data) ||
+      structuredDataPattern.test(data) ||
+      (data.length > 20 && nonNumericPattern.test(data)) // Ensure QR code has enough complexity
+    );
+  };
+
+  const fetchProductDetails = (barcode) => {
+    getDataByBarcode(barcode)
+      .then((product) => {
+        if (product) {
+          dispatch({ type: "SET_QR_DATA", payload: product });
+          // Navigate only if valid scan data is available
+          navigate("/scan-data", { state: { scannedData: product } });
+        } else {
+          console.error("Product not found for barcode:", barcode);
+        }
+      })
+      .catch((error) => {
+        console.error("Error fetching product details: ", error);
+      });
+  };
+
   return (
     <div className="flex flex-col min-h-screen">
       <div className="flex-grow">
@@ -150,13 +324,13 @@ const Home = () => {
         </button>
         <button
           className="w-full py-2 border border-blue-600 text-blue-600 font-semibold rounded-lg hover:bg-blue-100 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75 transition duration-200"
-          onClick={() => subscribeToMqttTopic("/a/b/c")}
+          onClick={() => subscribeToMqttTopic("emit/content/bleData")}
         >
           Subscribe to Topic
         </button>
         <button
           className="w-full py-2 border border-blue-600 text-blue-600 font-semibold rounded-lg hover:bg-blue-100 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75 transition duration-200"
-          onClick={() => publishMqttMessage("/a/b/c")}
+          onClick={() => publishMqttMessage("emit/content/bleData")}
         >
           Publish BLE Init Data
         </button>
