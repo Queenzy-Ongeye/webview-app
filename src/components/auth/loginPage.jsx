fd6ad288c7d4a2dcf6f3f883b3a65bbee53bdd79
Queenzy-Ongeye <queenzyblack176@gmail.com>
2024-10-07 19:45:39 +0300

    Added a login page

---
+++ src/components/auth/loginPage.jsx
@@ -0,0 +1,119 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const LoginPage = ({setIsAuthenticated}) => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const  [error, setError] = useState("");
+    const navigate  = useNavigate();
+
+    const handleLogin = async (e) => {
+        e.preventDefault();
+
+        if(!email || !password) {
+            setError("Please enter both email and password");
+            return;
+        };
+
+        if(email ==="oves.altec@omnivoltaic.com" && password === "Altec1234"){
+            localStorage.setItem("isAuthenticated", true);
+            setIsAuthenticated(true);
+            setError("");
+            navigate("/Header");
+        }else{
+            setError("Invalid email or password");
+        };
+    };
+
+  return (
+    <section class="bg-gray-50 mt-28 m-5">
+      <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+        <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
+          <img class="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg" alt="logo"/>
+          Flowbite    
+      </a>
+        <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 border-gray-700">
+          <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+            <h1 class="text-xl text-center font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
+              Welcome back<br></br>Sign in to your account
+            </h1>
+            <form class="space-y-4 md:space-y-6" action="#" onSubmit={handleLogin}>
+              <div>
+                <label
+                  for="email"
+                  class="block mb-2 text-sm font-medium text-gray-900"
+                >
+                  Your email
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  value={email}
+                  id="email"
+                  onChange={(e) => setEmail(e.target.value)}
+                  class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="name@company.com"
+                  required=""
+                />
+              </div>
+              <div>
+                <label
+                  for="password"
+                  class="block mb-2 text-sm font-medium text-gray-900"
+                >
+                  Password
+                </label>
+                <input
+                  type="password"
+                  name="password"
+                  id="password"
+                  placeholder="••••••••"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  required=""
+                />
+              </div>
+              <div class="flex items-center justify-between">
+                {/* <div class="flex items-start">
+                  <div class="flex items-center h-5">
+                    <input
+                      id="remember"
+                      aria-describedby="remember"
+                      type="checkbox"
+                      class="w-4 h-4 border border-gray-300 rounded bg-gray-200 focus:ring-3 focus:ring-primary-300 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
+                      required=""
+                    />
+                  </div>
+                  <div class="ml-3 text-sm">
+                    <label
+                      for="remember"
+                      class="text-gray-500 dark:text-gray-500"
+                    >
+                      Remember me
+                    </label>
+                  </div>
+                </div> */}
+                <a
+                  href="#"
+                  class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500"
+                >
+                  Forgot password?
+                </a>
+              </div>
+              {error && <p className="text-blue-500 text-center">{error}</p>}
+              <button
+                type="submit"
+                class="w-full text-white bg-blue-700 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+              >
+                Login
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default LoginPage;
