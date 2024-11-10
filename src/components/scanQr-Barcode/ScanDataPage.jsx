ac9b7f35ac6ac2cc0be38a94a7b23991240af496
Queenzy-Ongeye <queenzyblack176@gmail.com>
2024-11-10 23:16:01 +0300

    Added initBleData Handler

--- src/components/scanQr-Barcode/ScanDataPage.jsx
+++ src/components/scanQr-Barcode/ScanDataPage.jsx
@@ -222,37 +222,53 @@ const ScanDataPage = () => {
     }
   };
 
-  const connectToBluetoothDevice = async (macAddress) => {
-    return new Promise((resolve) => {
-      if (window.WebViewJavascriptBridge) {
-        window.WebViewJavascriptBridge.callHandler(
-          "connBleByMacAddress",
-          macAddress,
-          (responseData) => {
-            try {
-              const parsedData = JSON.parse(responseData);
-              if (parsedData.respCode === "200") {
-                initBleData(macAddress);
-              }
-              dispatch({ type: "SET_BLE_DATA", payload: parsedData });
-              resolve(parsedData.respCode === "200"); // Resolve true if successful
-            } catch (error) {
-              console.error(
-                "Error parsing JSON data from 'connBleByMacAddress' response:",
-                error
-              );
+  const connectToBluetoothDevice = (macAddress) => {
+    if (window.WebViewJavascriptBridge) {
+      window.WebViewJavascriptBridge.callHandler(
+        "connBleByMacAddress",
+        macAddress,
+        (responseData) => {
+          try {
+            const parsedData = JSON.parse(responseData);
+            if (parsedData.respCode === "200") {
+              initBleData(macAddress);
             }
+            dispatch({ type: "SET_BLE_DATA", payload: parsedData });
+            console.log("BLE Device Data:", parsedData);
+          } catch (error) {
+            console.error(
+              "Error parsing JSON data from 'connBleByMacAddress' response:",
+              error
+            );
           }
-        );
-      } else {
-        console.error("WebViewJavascriptBridge is not initialized.");
-      }
-    });
+        }
+      );
+    } else {
+      console.error("WebViewJavascriptBridge is not initialized.");
+    }
   };
 
-  // UI handling for matching status
-  const initBleData = async (macAddress) => {
-    return new Promise((resolve) => {});
+  const initBleData = (macAddress) => {
+    if (window.WebViewJavascriptBridge) {
+      window.WebViewJavascriptBridge.callHandler(
+        "initBleData",
+        macAddress,
+        (responseData) => {
+          try {
+            const parsedData = JSON.parse(responseData);
+            dispatch({ type: "SET_INIT_BLE_DATA", payload: parsedData });
+            console.log("BLE Init Data:", parsedData);
+          } catch (error) {
+            console.error(
+              "Error parsing JSON data from 'initBleData' response:",
+              error
+            );
+          }
+        }
+      );
+    } else {
+      console.error("WebViewJavascriptBridge is not initialized.");
+    }
   };
 
   // Search for a match in the BLE data once initialized
