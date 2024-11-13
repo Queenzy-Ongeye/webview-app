0dae3f9bb1a829cc7e05424512c0d43108136c90
Queenzy-Ongeye <queenzyblack176@gmail.com>
2024-11-13 14:02:54 +0300

    removed loaing state

--- src/components/scanQr-Barcode/ScanDataPage.jsx
+++ src/components/scanQr-Barcode/ScanDataPage.jsx
@@ -194,14 +194,14 @@ const ScanDataPage = () => {
         error
       );
       alert("Failed to connect and initialize BLE data. Please try again.");
-    } finally {
-      setConnectingMacAddress(null);
-      // Clear loading state for the specific device
       setLoadingMap((prevMap) => {
         const newMap = new Map(prevMap);
         newMap.set(macAddress, false);
         return newMap;
       });
+    } finally {
+      setConnectingMacAddress(null);
+      // Clear loading state for the specific device
     }
   };
 
