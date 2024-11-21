1bae490f08d792ae700f7d1a0a5dffc3dc673574
Queenzy-Ongeye <queenzyblack176@gmail.com>
2024-11-21 21:00:42 +0300

    mADE Changes

--- src/components/BleButtons/BleButtons.jsx
+++ src/components/BleButtons/BleButtons.jsx
@@ -86,15 +86,15 @@ const BleButtons = ({
   };
 
   const navigateToPage = (page) => {
-    console.log("Navigating with initBleDataResponse:", initBleDataResponse);
+    console.log("Navigating with initBleDataResponse:", initBleData.dataList);
   
-    if (!initBleDataResponse?.dataList || initBleDataResponse.dataList.length === 0) {
+    if (!initBleData?.dataList || initBleData.dataList.length === 0) {
       alert("No data to navigate with.");
       return;
     }
   
     // Pass dataList to BleDataPage
-    navigate(page, { state: initBleDataResponse.dataList });
+    navigate(page, { state: initBleData.dataList });
   };
   
 
@@ -105,7 +105,7 @@ const BleButtons = ({
 
   return (
     <div className="scan-data-page flex flex-col h-screen mt-6 w-full">
-      <div className="min-h-screen bg-gray-100">
+      <div className="min-h-screen bg-gray-100 w-full">
         <div className="p-2">
           {uniqueDevice.length > 0 ? (
             <ul className="text-left">
