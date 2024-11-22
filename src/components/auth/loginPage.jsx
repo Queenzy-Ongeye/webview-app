88b9921cee2df62e4c0950d32b748c3e8559b6b4
Queenzy-Ongeye <queenzyblack176@gmail.com>
2024-11-22 10:36:59 +0300

    added handle login func

--- src/components/auth/loginPage.jsx
+++ src/components/auth/loginPage.jsx
@@ -62,7 +62,7 @@ export default function LoginPage() {
           </CardDescription>
         </CardHeader>
         <CardContent>
-          <form onSubmit={handleLogin} className="space-y-4">
+          <form className="space-y-4">
             <div className="space-y-2">
               <Label htmlFor="email">Email</Label>
               <div className="relative">
@@ -112,7 +112,7 @@ export default function LoginPage() {
                 <AlertDescription>{error}</AlertDescription>
               </Alert>
             )}
-            <Button type="submit" className="w-full bg-oves-blue text-white">
+            <Button type="submit" className="w-full bg-oves-blue text-white" onClick={handleLogin}>
               Login
             </Button>
           </form>
